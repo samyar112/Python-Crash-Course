47b8752f4db1368cda37d8eb12c3abc9c0679369
Samir Pandey <samee.pan1994@gmail.com>
2024-05-04 00:51:28 -0700

    ..............................................................

---
+++ lists/guest_list.py
@@ -0,0 +1,8 @@
+guests = ['Sagar', 'Ashok', 'Bijay']
+message = "I would like to invite you to the dinner party. I hope you can make it."
+
+
+print(f"Hi {guests[0].upper()}, {message}")
+print(f"Hi {guests[-2].upper()}, {message}")
+print(f"Hi {guests[-1].upper()}, {message}")
+
---
+++ lists/names.py
@@ -0,0 +1,12 @@
+names = ['Steven Gerrard', 'Dominic Sobozlai', "Fernando Torres", 'Alison Becker', 'Trent Alexandar Arnold']
+
+print(names[0].lower())
+
+message = "Welcome to Liverpool Football Club"
+
+print(f"Hi! {names[0]}, {message}")
+print(f"Hi! {names[1]}, {message}")
+print(f"Hi! {names[2]}, {message}")
+print(f"Hi! {names[3]}, {message}")
+print(f"Hi! {names[4]}, {message}")
+
