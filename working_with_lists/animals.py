b72ce360d4a51ab310af67e696bd06b54fa64653
Samir Pandey <samee.pan1994@gmail.com>
2024-05-04 03:18:32 -0700

    Working with Lists

---
+++ working_with_lists/animals.py
@@ -0,0 +1,9 @@
+#Exercise 4-2
+
+animals = ['Dog', 'Cat', 'Rabbit']
+
+for animal in animals: 
+    
+    print(f"A {animal.lower()} would make a great pet.")
+
+print("\nAny of these animals would make a great pet!")
---
+++ working_with_lists/pizzas.py
@@ -0,0 +1,6 @@
+#Exercise 4-1
+pizza_types = ['Margherita', 'Pepperoni', 'Hawaiian', 'Vegetarian', 'Meat Lover']
+for pizza in pizza_types:
+    print(f"I like {pizza}. I am going to eat it this weekend with my wife.")
+
+print("\nI really love pizza! But I love my wife more.")
