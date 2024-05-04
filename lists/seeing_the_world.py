6d749f4534cca3e736d2bc4888f2ee25e2df48e9
Samir Pandey <samee.pan1994@gmail.com>
2024-05-04 02:55:41 -0700

    Organizing a list

---
+++ lists/seeing_the_world.py
@@ -0,0 +1,22 @@
+travel_destination = ['Hawai', 'Alaska', 'Nepal', 'Wichita', 'Mexico', 'France', 'Liverpool']
+print(travel_destination)
+
+print(sorted(travel_destination))
+
+print(travel_destination)
+
+print(sorted(travel_destination, reverse = True))
+
+print(travel_destination)
+
+travel_destination.reverse()
+print(travel_destination)
+
+travel_destination.reverse()
+print(travel_destination)
+
+travel_destination.sort()
+print(travel_destination)
+
+travel_destination.sort(reverse = True)
+print(travel_destination)
