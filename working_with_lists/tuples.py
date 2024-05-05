29ff1c29d20044e21208452c47f1f71926d96e43
Samir Pandey <samee.pan1994@gmail.com>
2024-05-05 02:50:53 -0700

    tuples

---
+++ working_with_lists/tuples.py
@@ -0,0 +1,13 @@
+#creating tuples
+buffet_menus = ('Chicken Tikka Masala', 'Chana Masala', 'Naan', 'Paneer Tikka Masala', 'Roti')
+for menu in buffet_menus:
+    print(menu)
+
+print(buffet_menus[0])
+
+#try to modify the items and show the error
+# buffet_menus[0] = 'Butter Chicken'
+
+buffet_menus = ('Chicken Tikka Masala', 'Saag Paneer', 'Aloo Saag', 'Paneer Tikka Masala', 'Roti')
+for menu in buffet_menus:
+    print(menu)
