a401da73d98c5ae7d026aef74328ad65767223a2
Samir Pandey <samee.pan1994@gmail.com>
2024-05-09 13:51:10 -0700

    if statements with lists

---
+++ if_statements.py/hello_admin.py
@@ -0,0 +1,11 @@
+#5-8
+
+admins = ["Sagar", "Ashok", "Bijay", "Rohit"]
+
+username = "Ashok"
+
+if username in admins:
+        print(f"Hello {username}, would you like to see a status report?")
+else:
+     print(f"Hello {username} , thank you for logging again.")
+
