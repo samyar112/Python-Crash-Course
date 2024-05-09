157a1fa76be1b319091561db0f45f2df9c4aa7b0
Samir Pandey <samee.pan1994@gmail.com>
2024-05-09 01:05:43 -0700

    more exercises

---
+++ lists/if_statements.py/favorite_fruit.py
@@ -0,0 +1,16 @@
+# 5-7
+favorite_fruits = ["black berries", "orange", "cherries"]
+
+if "black berries" in favorite_fruits:
+    print("You really like black berries. ")
+if "orange" in favorite_fruits:
+    print("You really like orange. ")
+if "cherries" in favorite_fruits:
+    print("You really like cherries. ")
+    #no output because its not on the list.
+if "apple" in favorite_fruits:
+    print("It's not your favorite fruit.")
+if "banana" in favorite_fruits:
+    print("It's not your favorite fruit.")
+
+
---
+++ lists/if_statements.py/stages_of_life.py
@@ -0,0 +1,17 @@
+# 5-6
+
+age = 2
+
+if age <2:
+    print("The person is a baby")
+elif 2 <= age < 4:
+    print("he person is a toddler")
+elif 4 <= age < 13:
+    print("The person is a kid")
+elif 13 <= age < 20:
+    print("The person is a teenager")
+elif 20 <= age < 65:
+    print("The person is a adult")
+elif age >= 65:
+    print("The person is an elder")
+    
