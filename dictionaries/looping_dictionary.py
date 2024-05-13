e07347b9bb5ddf09e6e00dd02da25912d3bfa6eb
Samir Pandey <samee.pan1994@gmail.com>
2024-05-12 20:41:16 -0700

    looping through dictionary

---
+++ dictionaries/looping_dictionary.py
@@ -0,0 +1,34 @@
+user_0 = {
+    'username': 'spandey',
+    'first': 'samir',
+    'last': 'pandey'
+}
+
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
+
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+#looping through key and value pair.
+for key, value in favorite_languages.items():
+  print(f"{key.title()} knows how to code in {value} language.\n")
+
+#looping through just the keys
+for name in favorite_languages.keys():
+   print(f"{name.title()}")
+
+friends = ['phil', 'sarah']
+
+for name in favorite_languages.keys():
+    print(f"{name.title()}")
+    if name in friends:
+      language = favorite_languages[name].title()
+      print(f"{name.title()}, I see you love {language}!")
+   
+   
