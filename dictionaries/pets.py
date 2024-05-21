4e9bb15cb4bf6a6f13438ed224dd521453d4d8bc
Samir Pandey <samee.pan1994@gmail.com>
2024-05-20 23:41:09 -0700

    cleanup

--- dictionaries/pets.py
+++ dictionaries/pets.py
@@ -1,24 +1,6 @@
 
 #6-8
 
-# # Create dictionaries representing different pets
-# pet1 = {"kind": "dog", "owner": "Alice"}
-# pet2 = {"kind": "cat", "owner": "Bob"}
-# pet3 = {"kind": "parrot", "owner": "Charlie"}
-
-# pets = [pet1, pet2, pet3]
-
-# # for pet in pets:
-# for keys, value in pet1.items():
-#      print(keys, value)
-# for keys, value in pet2.items():
-#     print(keys, value)
-# for keys, value in pet3.items():
-#     print(keys, value)
-    
-#     # for kind_pet, pet_details in pets.items():
-#         # print(f"Pet is : {pet_details}, Owner is: {pet_details}")
-# Make an empty list to store the pets in.
 pets = []
 
 # Make individual pets, and store each one in the list.
@@ -51,6 +33,7 @@ pets.append(pet)
 
 # Display information about each pet.
 for pet in pets:
-    print(f"\nHere's what I know about {pet['name'].title()}:")
-    for key, value in pet.items():
-        print(f"\t{key}: {value}")
+    print(f"Here's the information about {pet['name'].title()}\n")
+
+    for k, v in pet.items():
+        print(f"\t{k.title()}: {v}")
