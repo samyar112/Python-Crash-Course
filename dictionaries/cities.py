d753b9a0041f10e103c16f1b9b70f66232602e17
Samir Pandey <samee.pan1994@gmail.com>
2024-05-23 00:28:53 -0700

    exercise 6-11

--- dictionaries/cities.py
+++ dictionaries/cities.py
@@ -2,27 +2,35 @@
 
 cities = {
     'wichita': {
-        'country': 'usa',
-        'population': '396,192',
+        'country': 'USA',
+        'population': 396_192,
         'fact': 'Wichita is known as the "Air Capital of the World."'
     },
 
     'kathmandu': {
-        'country': 'nepal',
-        'population': '1,000,442',
+        'country': 'Nepal',
+        'population': 1_000_442,
         'fact': 'The city is one of the oldest continuously inhabited places in the world, founded in the 2nd century AD.'
     },
 
     'liverpool': {
-        'country': 'england',
-        'population': '496,784',
+        'country': 'England',
+        'population': 496_784,
         'fact': "The city's football club is the best in the world."
     }
 }
 
 for city, city_infos in cities.items():
     print(f"\nHere's some information about the {city.title()} city:")
+    
+    country = f"{city_infos['country']}"
+    population = "{:,}".format(city_infos['population'])
+    fact = f"{city_infos['fact']}"
 
+    print(f"Country: {country}") 
+    print(f"Population: {population}")
+    print(f"Fact: {fact}")
+# another way...
     for city_info, detail in city_infos.items():
-        print(f"\t{city_info.title()}: {detail.title()}")
+        print(f"\t{city_info.title()}: {detail}")
 
