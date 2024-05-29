0e3a712e8c92cde4e6015961ee3317f8d695963e
Samir Pandey <samee.pan1994@gmail.com>
2024-05-28 23:41:06 -0700

    break while loop

--- user_input_and_while_loops/flag.py
+++ user_input_and_while_loops/flag.py
@@ -24,3 +24,15 @@ while active:
     else:
         print(message.upper())
 
+#while loop break and continue
+
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.)"
+
+while True: 
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
