a1dc7817e210a2613178cebc687ddd5a1cf267b6
Samir Pandey <samee.pan1994@gmail.com>
2024-05-29 23:55:24 -0700

    continue and infinite loop

--- user_input_and_while_loops/flag.py
+++ user_input_and_while_loops/flag.py
@@ -36,3 +36,20 @@ while True:
         break
     else:
         print(f"I'd love to go to {city.title()}!")
+
+#continue loop
+
+current_number = 0
+while current_number <10:
+    current_number += 1 
+    if current_number % 2 == 0:
+        continue
+    print(current_number)
+
+
+#infinite loop
+
+x = 1
+while x <= 5:
+    print(x)
+    x += 1 
