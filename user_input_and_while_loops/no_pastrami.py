7d6a6c31138d977f025e60951d6900668cada7dc
Samir Pandey <samee.pan1994@gmail.com>
2024-06-08 23:08:35 -0700

    poll while loop

--- user_input_and_while_loops/no_pastrami.py
+++ user_input_and_while_loops/no_pastrami.py
@@ -16,3 +16,20 @@ while sandwich_orders:
 print(f"Here is the list of your confirmed orders of sandwiches:")
 for finished_sandwich in finished_sandwiches:
     print(f"{finished_sandwich.title()}")
+
+#7-10
+poll_result = {}
+polling_active = True
+while polling_active:
+    name = input("What's your name? ")
+    dream_vacation = input("If you could visit one place in the world,\n where would you go? ")
+
+    poll_result[name] = dream_vacation
+    repeat = input("Would you like to let another person respond?(yes/no) ")
+
+    if repeat =='no':
+        polling_active = False
+
+print("\n--- Poll Results ---")
+for name, response in poll_result.items():
+    print(f"{name.title()} has a dream to visit {response} someday.")
