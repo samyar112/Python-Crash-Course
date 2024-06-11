7c34f85c50c9070a3214230c87d0675f29af5d4e
Samir Pandey <samee.pan1994@gmail.com>
2024-06-10 23:54:16 -0700

    function paramaters

---
+++ functions/message.py
@@ -0,0 +1,13 @@
+#8.1
+def display_message():
+    print("We will be learning how to call function")
+
+display_message()
+
+#8.2
+def favorite_book(title):
+    print(f"One of my favorite books is {title}")
+
+title = "Alice in Wonderland"
+
+favorite_book(title)
