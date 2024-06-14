91d7dc4f8f0faaa8f84fddd42e799a1f9dcb47f2
Samir Pandey <samee.pan1994@gmail.com>
2024-06-13 23:45:19 -0700

    returning a dictionary

--- functions/formatted_name.py
+++ functions/formatted_name.py
@@ -24,3 +24,13 @@ musician3 = get_formatted_name("kurt", "cobain")
 
 print(musician2)
 print(musician3)
+
+def build_person(first_name, last_name, age = None):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+        return person
+    
+the_legend = build_person("jimi", "hendrix", age = 27)
+print(the_legend)
