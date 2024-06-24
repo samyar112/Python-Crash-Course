307be07b8f7a5feb7c3771f2c7e0c7f47959cd72
Samir Pandey <samee.pan1994@gmail.com>
2024-06-24 02:47:38 -0700

    function with while loop

--- functions/greeter.py
+++ functions/greeter.py
@@ -19,3 +19,24 @@ describe_pet("cat", "chato")
 describe_pet("dog", "willie")
 
 describe_pet("harry", "hampster")
+
+#using a function with a while loop
+#This is an infinite loop!
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    l_name = input ("Last name: ")
+    if l_name == 'q':
+        break
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
+
